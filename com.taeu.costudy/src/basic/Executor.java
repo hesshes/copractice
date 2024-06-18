df6545522543a4b42f8b95e0ee616f75e67d4452
ktaeu <hesshes@naver.com>
2024-06-18 19:35:33 +0900

    2024-06-18

--- com.taeu.costudy/src/basic/Executor.java
+++ com.taeu.costudy/src/basic/Executor.java
@@ -9,38 +9,47 @@ import basic.modules.Solution05;
 import basic.modules.Solution06;
 import basic.modules.Solution07;
 import basic.modules.Solution08;
+import basic.modules.Solution09;
+import basic.modules.Solution10;
 
 public class Executor {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		/*
-		 * 기초 1번 Solution01 se01 = new Solution01(); int[] nums = { 0, 1, 6, 4, 2, 7, 9,
-		 * 5 }; List answer = se01.solution(nums, 3);
-		 * System.out.println(answer.toString());
-		 */
+        /*
+         * 기초 1번 Solution01 se01 = new Solution01(); int[] nums = { 0, 1, 6, 4, 2, 7, 9,
+         * 5 }; List answer = se01.solution(nums, 3);
+         * System.out.println(answer.toString());
+         */
 
-		/*
-		 * 기초 3번 new Solution03(); se03.solution();
-		 */
+        /*
+         * 기초 3번 new Solution03(); se03.solution();
+         */
 
-		/*
-		 * 기초 4번 new Solution04(); se04.solution();
-		 */
+        /*
+         * 기초 4번 new Solution04(); se04.solution();
+         */
 
-		/*
-		 * 기초 5번 new Solution05().solution();
-		 */
+        /*
+         * 기초 5번 new Solution05().solution();
+         */
 
-		/*
-		 * 기초 6번 new Solution06().solution();
-		 */
+        /*
+         * 기초 6번 new Solution06().solution();
+         */
 
-		/*
-		 * 기초 7번 new Solution07().solution();
-		 */
+        /*
+         * 기초 7번 new Solution07().solution();
+         */
 
-		/* 기초 8번 */
-		new Solution08().solution();
-	}
+        /*
+         * 기초 8번 new Solution08().solution();
+         */
+        /* 기초 9번
+        new Solution09().solution();
+         */
+        
+        String test = new Solution10().solution("He11oWor1d", "lloWorl", 2);
+        System.out.println(test);
+    }
 
 }
