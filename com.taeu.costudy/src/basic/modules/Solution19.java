f42edb9f18b8a4e2913825cf9729a5d161aec708
taeu <hesshes@naver.com>
2024-07-06 21:33:22 +0900

    2024-07-06

---
+++ com.taeu.costudy/src/basic/modules/Solution19.java
@@ -0,0 +1,45 @@
+package basic.modules;
+
+public class Solution19 {
+	/*
+	 * 문자열에 따라 다음과 같이 두 수의 크기를 비교하려고 합니다.
+	 * 
+	 * 두 수가 n과 m이라면
+	 * 
+	 * ">", "=" : n >= m
+	 * 
+	 * "<", "=" : n <= m
+	 * 
+	 * ">", "!" : n > m
+	 * 
+	 * "<", "!" : n < m 두 문자열 ineq와 eq가 주어집니다.
+	 * 
+	 * ineq는 "<"와 ">"중 하나고, eq는 "="와 "!"중 하나입니다. 그리고 두 정수 n과 m이 주어질 때,
+	 * 
+	 * n과 m이 ineq와 eq의 조건에 맞으면 1을 아니면 0을 return하도록
+	 * 
+	 * 
+	 * 제한 사항
+	 * 
+	 * 1 ≤ n, m ≤ 100 solution 함수를 완성해주세요.
+	 * 
+	 **/
+
+	public int solution(String ineq, String eq, int n, int m) {
+		int answer = 0;
+		if (ineq.equals(">") && eq.equals("=")) {
+			answer = n >= m ? 1 : 0;
+		} else if (ineq.equals("<") && eq.equals("=")) {
+			answer = n <= m ? 1 : 0;
+		} else if (ineq.equals(">") && eq.equals("!")) {
+			answer = n > m ? 1 : 0;
+		} else if (ineq.equals("<") && eq.equals("!")) {
+			answer = n < m ? 1 : 0;
+		}
+		// 프로그래머스에서 적당한 레퍼런스를 찾지 못함
+		// 좀 더 간략하게 코드를 짤 수 있는걸 찾아보는중
+		return answer;
+
+	}
+
+}
---
+++ com.taeu.costudy/src/basic/modules/Solution22.java
@@ -0,0 +1,36 @@
+package basic.modules;
+
+public class Solution22 {
+
+	// 두 정수 a,d와 길이가 n인 boolean 배열 included가 주어집니다.
+	// 첫째항이 a, 공차가 d인 등차수열에서 included[i] 가 i+1 항을 의미할 때,
+	// 이 등차수열의 1항부터 n항까지 included가
+	// true인 항들만 더한 값을 return 하는
+	// solution 함수를 작성해 주세요.
+
+	// 제한사항
+	// 1 ≤ a ≤ 100
+	// 1 ≤ d ≤ 100
+	// 1 ≤ included의 길이 ≤ 100
+	// included에는 true가 적어도 하나 존재합니다.
+
+	// a+(n−1)d
+
+	// a + (n-1)d =
+	public int solution(int a, int d, boolean[] included) {
+		int result = 0;
+		if (a > 0 && a < 101 && d > 0 && d < 101 && included.length > 0 && included.length < 101) {
+
+			for (int i = 1; i < included.length + 1; i++) {
+				if (included[i - 1])
+					result += (a + (i - 1) * d);
+			}
+		}
+		return result;
+		// 프로그래머스 정답
+		// IntStream.range(0, included.length).map(idx ->
+		// included[idx]?a+(idx*d):0).sum();
+		// Stream 사용법 정리하자
+	}
+
+}
